c6acd8e62522e7f19ed51f641ca72f043c3f2fef
David Alvarez <davalres08@gmail.com>
2019-09-26 12:41:37 +0200

    in app.component.ts, Make sure store subscription is done after store initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Store, createAction } from '@ngrx/store';
 import { AppState } from './app.state';
 import { Storage } from '@ionic/storage';
 import { AppInit } from './ngx-store/actions/app.action';
+import { Observable, Subscription } from 'rxjs';
+import { LoggerDispatcherService } from './services/logger-dispatcher.service';
 
 @Component({
   selector: 'app-root',
@@ -18,7 +20,8 @@ export class AppComponent implements OnInit {
         private storage: Storage,
         private platform: Platform,
         private splashScreen: SplashScreen,
-        private statusBar: StatusBar
+        private statusBar: StatusBar,
+        private logger: LoggerDispatcherService,
     ) {
         this.initializeApp();
     }
@@ -33,14 +36,36 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.storage.get('state').then((stringState) => {
+        // TODO: Check if now there is no more problems when killing the app.
+        // Before the app state was not correct when restarting the app.
+        this.dispatchStorageInitState()
+            .then(() => {
+                this.subscribeToStoreChanges();
+            });
+    }
+
+    /**
+     * Takes the global state of the app from the local storage and put it inside the ngrx-store
+     */
+    private async dispatchStorageInitState(): Promise<void> {
+        return this.storage.get('state').then((stringState) => {
             const state = JSON.parse(stringState);
             this.store.dispatch(new AppInit(state));
+            this.logger.log(`STATE OF THE APP HAS BEEN INITIALIZED IN NGRX-STORE`);
         });
+    }
+
+    /**
+     * Listens to ngrx-store state changes and saves it in local storage
+     */
+    private subscribeToStoreChanges(): void {
         this.store.subscribe((state) => {
             const stringState = JSON.stringify(state);
             this.storage.set('state', stringState).then(() => {
                 console.log('STATE PUT IN LOCAL STORAGE');
+            })
+            .then(() => {
+                this.logger.log(`NGRX-STORE HAS BEEN SAVED IN LOCAL STORAGE`);
             });
         });
     }
